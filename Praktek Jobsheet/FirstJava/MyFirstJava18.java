867e0070fc1058be0ed00eefbe9464bf53727bfc
Muhfarq <mhmmadfarq7@gmail.com>
2023-11-22 11:03:33 +0700

    push1

--- Praktek Jobsheet/FirstJava/MyFirstJava18.java
+++ Praktek Jobsheet/FirstJava/MyFirstJava18.java
@@ -2,6 +2,10 @@ public class MyFirstJava18 {
     public static void main (String[] args) {
         System.out.print ( "\nmy name is faruq");
         
+        System.out.print ( "\nmy name is faruq1");
+        System.out.print ( "\nmy name is faruq2");
+        System.out.print ( "\nmy name is faruq3");
+        System.out.print ( "\nmy name is faruq4");
         
     }
 }
